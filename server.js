92d5e89b3804f50bac0999b921785f881176bb29
Jefferson Clark <jeffersonclarkti@gmail.com>
2020-07-08 11:17:20 -0300

    Curso de NODEjs Api - Rocketseat

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors')
+const mongoose = require('mongoose');
+const requireDir = require('require-dir')
+
+// Iniciando o APP
+const app = express();
+app.use(express.json());
+app.use(cors()); // Publicar API
+
+// Iniciando Mongo
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi', 
+    {useNewUrlParser: true, useUnifiedTopology: true}
+);
+
+requireDir('./src/Models');
+
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+const exppress = require('express');
+const routes = exppress.Router();
+
+const ProductControllers = require('./controllers/ProductController');
+
+routes.get('/products', ProductControllers.index);
+routes.get('/products/:id', ProductControllers.show);
+routes.post('/products', ProductControllers.store);
+routes.put('/products/:id', ProductControllers.update);
+routes.delete('/products/:id', ProductControllers.destroy);
+
+module.exports = routes;
+
